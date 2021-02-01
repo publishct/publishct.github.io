e21dc9a507ca3b147010ed6f2f6594c02a6df4d8
Cynthia Trujillo <developercynthia@gmail.com>
2021-02-01 12:54:22 -0600

    Sweep name change

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 const myLinkedInBtn = document.querySelector('.linkedInContainer');
 const linkedInLink = document.createElement('a');
 
-linkedInLink.setAttribute('href', 'https://www.linkedin.com/in/cynthiathey/');
-linkedInLink.setAttribute ('target','_blank');
+linkedInLink.setAttribute('href', 'https://linkedin.com/in/1codylake');
+linkedInLink.setAttribute('target', '_blank');
 linkedInLink.setAttribute('class', 'btn btn-primary btn-sm');
-linkedInLink.setAttribute('role','button');
+linkedInLink.setAttribute('role', 'button');
 linkedInLink.textContent = 'LinkedIn';
 
 myLinkedInBtn.appendChild(linkedInLink);
@@ -14,21 +14,21 @@ const mygithubBtn = document.querySelector('.githubContainer');
 const githubLink = document.createElement('a');
 
 githubLink.setAttribute('href', 'https://github.com/publishct');
-githubLink.setAttribute ('target','_blank');
+githubLink.setAttribute('target', '_blank');
 githubLink.setAttribute('class', 'btn btn-primary btn-sm');
-githubLink.setAttribute('role','button');
+githubLink.setAttribute('role', 'button');
 githubLink.textContent = 'github';
 
 mygithubBtn.appendChild(githubLink);
 
 const seeMore = document.querySelector('.collapseLive');
-seeMore.textContent = 'see Cynthia\'s live learning samples'
+seeMore.textContent = 'see Cody\'s live learning samples'
 
-function textChange (e) {
-    if ( e.target.textContent === 'see Cynthia\'s live learning samples' ) {
-        e.target.textContent =  '...see fewer samples';
+function textChange(e) {
+    if (e.target.textContent === 'see Cody\'s live learning samples') {
+        e.target.textContent = '...see fewer samples';
     } else {
-        e.target.textContent = 'see Cynthia\'s live learning samples';
+        e.target.textContent = 'see Cody\'s live learning samples';
     }
 }
 
